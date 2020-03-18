f5cf313696b248556d33a820bdd65c1291482d3a
Mary Etokwudo <etokwudomary@gmail.com>
2020-03-18 18:49:23 +0100

    update post

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -3,7 +3,7 @@
     <label for="cat_title"> Edit Category</label>
 
     <?php
-    // edit category
+    // fetch category data from the database
     if (isset($_GET["edit"])) {
       $cat_id = $_GET["edit"];
       $query = "SELECT * FROM categories WHERE cat_id = {$cat_id} ";
