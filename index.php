85470ed663c277ce72c5ac748a5575a3e7f3b224
Mary Etokwudo <etokwudomary@gmail.com>
2020-03-19 12:16:38 +0100

    add individual post page and link

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ include "includes/navbar.php";
             $query = "SELECT * FROM posts";
             $all_posts = mysqli_query($connection, $query);
             while ($row = mysqli_fetch_assoc($all_posts)) {
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -35,14 +36,15 @@ include "includes/navbar.php";
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
+
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
