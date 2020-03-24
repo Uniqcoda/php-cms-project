b4c480c38d875ad962a7451ba210c69ac58638f4
Mary Etokwudo <etokwudomary@gmail.com>
2020-03-24 10:22:47 +0100

    view all users from the admin dashboard

---
+++ admin/includes/view_all_users.php
@@ -0,0 +1,74 @@
+<table class="table table-hover table-bordered">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Username</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      <th>Role</th>
+      <!-- <th>Image</th> -->
+      <th>Approve</th>
+      <th>Unapprove</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <?php
+      global $connection;
+      $query = "SELECT * FROM users ";
+      // $query .= "ORDER BY user_id DESC ";
+      $result = mysqli_query($connection, $query);
+      while ($row = mysqli_fetch_assoc($result)) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
+
+        echo "<tr>";
+        echo "<td>$user_id</td>";
+        echo "<td>$username</td>";
+        echo "<td>$user_firstname</td>";
+        echo "<td>$user_lastname</td>";
+        echo "<td>$user_email</td>";
+
+        echo "<td>$user_role</td>";
+        // echo "<td>$user_image</td>";
+        echo "<td><a href='users.php?approve={$user_id}'>Approve</a></td>";
+        echo "<td><a href='users.php?unapprove={$user_id}'>Unapprove</a></td>";
+        echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
+        echo "</tr>";
+      } ?>
+    </tr>
+  </tbody>
+</table>
+
+<?php
+if (isset($_GET["approve"])) {
+  $user_id = $_GET["approve"];
+  $query = "UPDATE users SET comment_status = 'approved' WHERE user_id = {$user_id} ";
+  $result = mysqli_query($connection, $query);
+  confirmQuery($result);
+  header("Location: users.php");
+}
+
+if (isset($_GET["unapprove"])) {
+  $user_id = $_GET["unapprove"];
+  $query = "UPDATE users SET comment_status = 'unapproved' WHERE user_id = {$user_id} ";
+  $result = mysqli_query($connection, $query);
+  confirmQuery($result);
+  header("Location: users.php");
+}
+
+if (isset($_GET["delete"])) {
+  $user_id = $_GET["delete"];
+  $query = "DELETE FROM users WHERE user_id = {$user_id} ";
+  $result = mysqli_query($connection, $query);
+  confirmQuery($result);
+  header("Location: users.php");
+}
+?>
