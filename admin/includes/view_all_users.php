fef137d2361f877f7f2af8d0734faa76d02e9824
Mary Etokwudo <etokwudomary@gmail.com>
2020-03-24 13:24:38 +0100

    create add user form

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -8,8 +8,8 @@
       <th>Email</th>
       <th>Role</th>
       <!-- <th>Image</th> -->
-      <th>Approve</th>
-      <th>Unapprove</th>
+      <th>Make Admin</th>
+      <th>Make Subscriber</th>
       <th>Delete</th>
     </tr>
   </thead>
@@ -38,8 +38,8 @@
 
         echo "<td>$user_role</td>";
         // echo "<td>$user_image</td>";
-        echo "<td><a href='users.php?approve={$user_id}'>Approve</a></td>";
-        echo "<td><a href='users.php?unapprove={$user_id}'>Unapprove</a></td>";
+        echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
+        echo "<td><a href='users.php?change_to_sub={$user_id}'>Subscriber</a></td>";
         echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
         echo "</tr>";
       } ?>
@@ -48,17 +48,17 @@
 </table>
 
 <?php
-if (isset($_GET["approve"])) {
-  $user_id = $_GET["approve"];
-  $query = "UPDATE users SET comment_status = 'approved' WHERE user_id = {$user_id} ";
+if (isset($_GET["change_to_admin"])) {
+  $user_id = $_GET["change_to_admin"];
+  $query = "UPDATE users SET user_role = 'admin' WHERE user_id = {$user_id} ";
   $result = mysqli_query($connection, $query);
   confirmQuery($result);
   header("Location: users.php");
 }
 
-if (isset($_GET["unapprove"])) {
-  $user_id = $_GET["unapprove"];
-  $query = "UPDATE users SET comment_status = 'unapproved' WHERE user_id = {$user_id} ";
+if (isset($_GET["change_to_sub"])) {
+  $user_id = $_GET["change_to_sub"];
+  $query = "UPDATE users SET user_role = 'subscriber' WHERE user_id = {$user_id} ";
   $result = mysqli_query($connection, $query);
   confirmQuery($result);
   header("Location: users.php");
