5f4b168e3d0893df23a60daa66807b336545adea
Mary Etokwudo <etokwudomary@gmail.com>
2020-03-26 13:56:35 +0100

    validating users with admin role

--- includes/login.php
+++ includes/login.php
@@ -23,17 +23,15 @@ if (isset($_POST["login"])) {
     $db_user_role = $row["user_role"];
   }
 
-  if ($username !== $db_username && $password !== $db_user_password) {
+  if ($password !== $db_user_password) {
     header("Location: ../index.php");
-  } else if ($username == $db_username && $password == $db_user_password) {
-    $_SESSION["username"]= $db_username;
-    $_SESSION["firstname"]= $db_user_firstname;
-    $_SESSION["lastname"]= $db_user_lastname;
-    $_SESSION["user_role"]= $db_user_role;
+  } else if ($password == $db_user_password) {
+    $_SESSION["username"] = $db_username;
+    $_SESSION["firstname"] = $db_user_firstname;
+    $_SESSION["lastname"] = $db_user_lastname;
+    $_SESSION["user_role"] = $db_user_role;
 
     header("Location: ../admin");
-  } else {
-    header("Location: ../index.php");
   }
 }
 
