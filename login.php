990da35f3e10de6efaf79ed1d204bde1f8be662f
Mary Etokwudo <etokwudomary@gmail.com>
2020-03-28 17:48:23 +0100

    add comment if user is verified

--- login.php
+++ login.php
@@ -30,6 +30,7 @@ if (isset($_POST["submit"])) {
       $db_hashed_password = $row["user_password"];
       $db_user_firstname = $row["user_firstname"];
       $db_user_lastname = $row["user_lastname"];
+      $db_user_email = $row["user_email"];
       $db_user_role = $row["user_role"];
 
 
@@ -39,6 +40,7 @@ if (isset($_POST["submit"])) {
         $_SESSION["username"] = $db_username;
         $_SESSION["firstname"] = $db_user_firstname;
         $_SESSION["lastname"] = $db_user_lastname;
+        $_SESSION["email"] = $db_user_email;
         $_SESSION["user_role"] = $db_user_role;
 
         header("Location: index.php");
--- post.php
+++ post.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "includes/header.php";
 include "includes/db.php";
 include "admin/functions.php";
@@ -14,136 +15,130 @@ include "includes/navbar.php";
 <!-- Page Content -->
 <div class="container">
 
-    <div class="row">
+  <div class="row">
 
-        <!-- Blog Sidebar Widgets Column -->
-        <?php
-        include "includes/sidebar.php";
-        ?>
+    <!-- Blog Sidebar Widgets Column -->
+    <?php
+    include "includes/sidebar.php";
+    ?>
 
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-            <?php
-            if (isset($_GET["p_id"])) {
-                $post_id = $_GET["p_id"];
-            }
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
+      <?php
+      if (isset($_GET["p_id"])) {
+        $post_id = $_GET["p_id"];
+      }
 
-            $query = "SELECT * FROM posts WHERE post_id = $post_id ";
-            $all_posts = mysqli_query($connection, $query);
-            while ($row = mysqli_fetch_assoc($all_posts)) {
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = $row['post_content'];
-            ?>
+      $query = "SELECT * FROM posts WHERE post_id = $post_id ";
+      $all_posts = mysqli_query($connection, $query);
+      while ($row = mysqli_fetch_assoc($all_posts)) {
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_date = $row['post_date'];
+        $post_image = $row['post_image'];
+        $post_content = $row['post_content'];
+      ?>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <!-- First Blog Post -->
+        <h2>
+          <a href="#"><?php echo $post_title; ?></a>
+        </h2>
+        <p class="lead">
+          by <a href="index.php"><?php echo $post_author ?></a>
+        </p>
+        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+        <hr>
+        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+        <hr>
+        <p><?php echo $post_content; ?></p>
 
-                <hr>
-            <?php
-            }
-            ?>
+        <hr>
+      <?php
+      }
+      ?>
 
-            <!-- Blog Comments -->
-            <?php
-            if (isset($_POST["create_comment"])) {
-                $comment_post_id = $_GET["p_id"];
-                $comment_author = $_POST["comment_author"];
-                $comment_email = $_POST["comment_email"];
-                $comment_content = $_POST["comment_content"];
+      <!-- Blog Comments -->
+      <?php
+      if (isset($_SESSION["username"])) {
+        if (isset($_POST["create_comment"])) {
+          $comment_post_id = $_GET["p_id"];
+          $comment_author = $_SESSION["firstname"] . " " . $_SESSION["lastname"];
+          $comment_email = $_SESSION["email"];
+          $comment_content = $_POST["comment_content"];
 
-                $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date ) ";
-                $query .= "VALUE ($comment_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now()) ";
+          $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date ) ";
+          $query .= "VALUE ($comment_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now()) ";
 
-                $comment_count_query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-                $comment_count_query .= "WHERE post_id = $comment_post_id ";
-                $result = mysqli_query($connection, $query);
-                $result_comment_count_query = mysqli_query($connection, $comment_count_query);
-                confirmQuery($result);
-                if (!$result || !$result_comment_count_query) {
-                    die("QUERY FAILED " . mysqli_error($connection));
-                }
-            }
-            ?>
+          $comment_count_query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+          $comment_count_query .= "WHERE post_id = $comment_post_id ";
+          $result = mysqli_query($connection, $query);
+          $result_comment_count_query = mysqli_query($connection, $comment_count_query);
+          confirmQuery($result);
+          if (!$result || !$result_comment_count_query) {
+            die("QUERY FAILED " . mysqli_error($connection));
+          }
+        }
+      ?>
 
-            <!-- Comments Form -->
-            <div class="well">
-                <h4>Leave a Comment:</h4>
-                <form action="" method="post" role="form">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="comment_author" placeholder="Enter Full Name">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" name="comment_email" placeholder="Enter Email Address">
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" rows="3" name="comment_content" placeholder="Enter Comment"></textarea>
-                    </div>
-                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                </form>
+        <!-- Comments Form -->
+        <div class="well">
+          <h4>Leave a Comment:</h4>
+          <form action="" method="post" role="form">
+            <div class="form-group">
+              <textarea class="form-control" rows="3" name="comment_content" placeholder="Enter Comment"></textarea>
             </div>
-
-            <hr>
-
-            <!-- Posted Comments -->
-
-            <!--Display Comments -->
-            <?php
-            if (isset($_GET['p_id'])) {
-                $post_id = $_GET['p_id'];
-
-                $comment_query = "SELECT * FROM comments WHERE comment_post_id = $post_id ";
-                $comment_query .= "AND comment_status = 'approved' ";
-                // newer comments first
-                $comment_query .= "ORDER BY comment_date DESC";
-                $comment_result = mysqli_query($connection, $comment_query);
-                confirmQuery($comment_result);
-
-                while ($row = mysqli_fetch_assoc($comment_result)) {
-                    $comment_id = $row['comment_id'];
-                    $comment_post_id = $row['comment_post_id'];
-                    $comment_author = $row['comment_author'];
-                    $comment_content = substr($row['comment_content'], 0, 100);
-                    $comment_email = $row['comment_email'];
-                    $comment_status = $row['comment_status'];
-                    $comment_date = $row['comment_date'];
-            ?>
-
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading"><?php echo $comment_author ?> <small> <?php echo $comment_date ?></small>
-                            </h4>
-                            <p><?php echo $comment_content ?></p>
-                        </div>
-                    </div>
-
-            <?php
-                }
-            }
-            ?>
+            <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
+          </form>
         </div>
 
-    </div>
-    <!-- /.row -->
+        <hr>
+      <?php
+      }
+      ?>
+      <!--Display Comments -->
+      <?php
+      if (isset($_GET['p_id'])) {
+        $post_id = $_GET['p_id'];
 
-    <hr>
+        $comment_query = "SELECT * FROM comments WHERE comment_post_id = $post_id ";
+        $comment_query .= "AND comment_status = 'approved' ";
+        // newer comments first
+        $comment_query .= "ORDER BY comment_date DESC";
+        $comment_result = mysqli_query($connection, $comment_query);
+        confirmQuery($comment_result);
+
+        while ($row = mysqli_fetch_assoc($comment_result)) {
+          $comment_id = $row['comment_id'];
+          $comment_post_id = $row['comment_post_id'];
+          $comment_author = $row['comment_author'];
+          $comment_content = substr($row['comment_content'], 0, 100);
+          $comment_email = $row['comment_email'];
+          $comment_status = $row['comment_status'];
+          $comment_date = $row['comment_date'];
+      ?>
+
+          <div class="media">
+            <a class="pull-left" href="#">
+              <img class="media-object" src="http://placehold.it/64x64" alt="">
+            </a>
+            <div class="media-body">
+              <h4 class="media-heading"><?php echo $comment_author ?> <small> <?php echo $comment_date ?></small>
+              </h4>
+              <p><?php echo $comment_content ?></p>
+            </div>
+          </div>
+
+      <?php
+        }
+      }
+      ?>
+    </div>
+
+  </div>
+  <!-- /.row -->
+
+  <hr>
 </div>
 <?php
 include "includes/footer.php";
