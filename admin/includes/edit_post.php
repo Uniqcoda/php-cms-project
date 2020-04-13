a55169f75638d83abbbf97c7c226c5dd8640ce56
Mary Etokwudo <etokwudomary@gmail.com>
2020-04-13 13:58:53 +0100

    add post update notification

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -2,6 +2,50 @@
 include "../functions.php";
 ?>
 
+<?php
+// update query
+if (isset($_POST["update_post"])) {
+
+  $post_id = $_GET["p_id"];
+  $post_title = $_POST['title'];
+  $post_author = $_POST['author'];
+  $post_category_id  = $_POST['post_category_id'];
+  $post_status = $_POST['post_status'];
+  $post_image = $_FILES['image']['name'];
+  $post_image_temp = $_FILES['image']['tmp_name'];
+  $post_tags = $_POST['post_tags'];
+  $post_content = $_POST['post_content'];
+
+  move_uploaded_file($post_image_temp, "../images/$post_image");
+
+  // fix bug: submitting empty image value
+  if (empty($post_image)) {
+    $query = "SELECT * FROM posts WHERE post_id = {$post_id} ";
+
+    $result = mysqli_query($connection, $query);
+    confirmQuery($result);
+    while ($row = mysqli_fetch_assoc($result)) {
+      $post_image = $row['post_image'];
+    }
+  }
+
+  $query = "UPDATE posts SET ";
+  $query .= "post_title = '{$post_title}', ";
+  $query .= "post_category_id = '{$post_category_id}', ";
+  $query .= "post_date = now(), ";
+  $query .= "post_author = '{$post_author}', ";
+  $query .= "post_status = '{$post_status}', ";
+  $query .= "post_tags = '{$post_tags}', ";
+  $query .= "post_content = '{$post_content}', ";
+  $query .= "post_image = '{$post_image}' ";
+  $query .= "WHERE post_id = {$post_id} ";
+
+  $result = mysqli_query($connection, $query);
+  confirmQuery($result);
+  // header("Location: posts.php");
+  echo "<p>Post updated. <a href='../post.php?p_id=$post_id'> View Post </a></p>";
+}
+?>
 
 <h3>Edit Post</h3>
 <form action="" method="post" enctype="multipart/form-data">
@@ -28,7 +72,6 @@ include "../functions.php";
       $post_date = $row['post_date'];
       $post_comment_count = $row['post_comment_count'];
   ?>
-
 
       <div class="form-group">
         <label for="title">Post Title</label>
@@ -89,53 +132,8 @@ include "../functions.php";
         <textarea class="form-control " name="post_content" id="body" cols="30" rows="10"><?php if (isset($post_content)) echo $post_content ?></textarea>
       </div>
 
-
   <?php }
   } ?>
-
-  <?php
-  // update query
-  if (isset($_POST["update_post"])) {
-
-    $post_id = $_GET["p_id"];
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id  = $_POST['post_category_id'];
-    $post_status = $_POST['post_status'];
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-
-    move_uploaded_file($post_image_temp, "../images/$post_image");
-
-    // fix bug: submitting empty image value
-    if (empty($post_image)) {
-      $query = "SELECT * FROM posts WHERE post_id = {$post_id} ";
-
-      $result = mysqli_query($connection, $query);
-      confirmQuery($result);
-      while ($row = mysqli_fetch_assoc($result)) {
-        $post_image = $row['post_image'];
-      }
-    }
-
-    $query = "UPDATE posts SET ";
-    $query .= "post_title = '{$post_title}', ";
-    $query .= "post_category_id = '{$post_category_id}', ";
-    $query .= "post_date = now(), ";
-    $query .= "post_author = '{$post_author}', ";
-    $query .= "post_status = '{$post_status}', ";
-    $query .= "post_tags = '{$post_tags}', ";
-    $query .= "post_content = '{$post_content}', ";
-    $query .= "post_image = '{$post_image}' ";
-    $query .= "WHERE post_id = {$post_id} ";
-
-    $result = mysqli_query($connection, $query);
-    confirmQuery($result);
-    header("Location: posts.php");
-  }
-  ?>
 
   <div class="form-group">
     <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
