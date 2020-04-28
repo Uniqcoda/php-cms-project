99750262b0ffe7216b6b25147bd018e8f6e817b2
Mary Etokwudo <etokwudomary@gmail.com>
2020-04-28 15:27:19 +0100

    add a loader to the CMS admin

--- author_posts.php
+++ author_posts.php
@@ -48,9 +48,6 @@ include "includes/navbar.php";
           <h2>
             <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
           </h2>
-          <p class="lead">
-            by <<a href='author_posts.php?author=<?php echo $post_author; ?>'><?php echo $post_author ?></a>
-          </p>
           <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
           <hr>
           <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
